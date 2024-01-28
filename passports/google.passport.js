a3481c8e5a786ec1c89bfe09c23187a1fb0e9b94
Anh Khoa <mr.mrkhoacoding@gmail.com>
2024-01-29 04:34:35 +0700

    fix bug

--- passports/google.passport.js
+++ passports/google.passport.js
@@ -4,7 +4,7 @@ module.exports = new GoogleStrategy(
     {
       clientID: "1048703701057-u6c8ottrdve08q7smn2mrr2o920t7lon.apps.googleusercontent.com",
       clientSecret: "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",
-      callbackURL: `${process.env.HOSTING}/auth/google/callback`,
+      callbackURL: `https://node-auth-pro.vercel.app/auth/google/callback`,
       scope: ["profile", "email"],
     },
     function (request, accessToken, refreshToken, profile, done) {
